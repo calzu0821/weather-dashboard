0f947aaab94a358caf9c628878d973f832623977
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-05-06 19:43:50 -0400

    created variables and a query URL to make the API call

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// Created variables for the API call.
+const APIkey = "5e7c300090fc006e727aed7871c6a2eb";
+var city;
+
+// Constructed a Query URL to make the API call. Used string concatenation to do this.
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+
+// Make the API call using fetch
+
