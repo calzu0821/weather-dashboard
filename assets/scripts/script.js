a6446bb0cf99720d2dc14ceefd071a31039e83cd
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-05-11 14:54:06 -0400

    adding variables and looping localstorage to the UI

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,3 +13,32 @@ $(document).ready(function () {
         city.textContent = "";
         });
       }
+
+    //declaring variables and looping the local storage to the UI
+    let cityOne = document.getElementById('city-1')
+    let cityTwo = document.getElementById('city-2')
+    let cityThree = document.getElementById('city-3')
+    let cityFour = document.getElementById('city-4')
+    let cityFive = document.getElementById('city-5')
+    let citySix = document.getElementById('city-6')
+    let citySeven = document.getElementById('city-7')
+    let cityEight = document.getElementById('city-8')
+    
+    let history = JSON.parse(localStorage.getItem("history"));
+
+    if (history !== null && history !== undefined) {
+    for (let i = 0; i < history.length; i++) {
+    let city = document.getElementById(`city-${i+1}`);
+      city.onclick = function() {
+      searchWeather(history[i]);
+    };
+    if (history.length > 0) cityOne.textContent = history[0];
+    if (history.length > 1) cityTwo.textContent = history[1];
+    if (history.length > 2) cityThree.textContent = history[2];
+    if (history.length > 3) cityFour.textContent = history[3];
+    if (history.length > 4) cityFive.textContent = history[4];
+    if (history.length > 5) citySix.textContent = history[5];
+    if (history.length > 6) citySeven.textContent = history[6];
+    if (history.length > 7) cityEight.textContent = history[7];
+  }
+}
