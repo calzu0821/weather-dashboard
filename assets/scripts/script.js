60a17860fea5b0686dfd715f54e6e2c7fd3bf00b
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-05-11 14:52:41 -0400

    added clear button function in JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,15 @@
-// Created variables for the API call.
-const APIkey = "5e7c300090fc006e727aed7871c6a2eb";
-var city;
+$(document).ready(function () {
 
-// Constructed a Query URL to make the API call. Used string concatenation to do this.
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+    //clear history button function from localstorage
+    let clearHistory = document.getElementById('clear-history');
+    clearHistory.onclick = function() {
+    localStorage.clear();
+    clearSearchHistoryUI();
+    };
 
-// Make the API call using fetch
-fetch(queryURL)
-    .then(response => response.json())
-    .then(data => {
-    // Code to handle the API response goes here
-    console.log(data); // Example: log the data to the console
-    })
-    .catch(error => {
-    // Code to handle errors goes here
-    console.error(error); // Example: log the error to the console
-    });
+    function clearSearchHistoryUI() {
+        let cityElements = [cityOne, cityTwo, cityThree, cityFour, cityFive, citySix, citySeven, cityEight];
+        cityElements.forEach((city) => {
+        city.textContent = "";
+        });
+      }
